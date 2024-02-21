e8a1f8e58f8f8fc6ee080f6f3a98cb840f35b980
NoorTushar <tushar.noor.khan@gmail.com>
2024-02-21 20:05:29 +0600

    arrow functions

--- module-30-ES6/arrow.js
+++ module-30-ES6/arrow.js
@@ -1,18 +1,18 @@
 // Arrow Functions
 
-// this is traditional function declaration:
+// 1. this is traditional function declaration: anonymous function
 
 function minus(a, b) {
    return a - b;
 }
 
-// this is function declaration:
+// 2. this is function expression: keeping it in a variable
 
 const multiplication = function (a, b) {
    return a * b;
 };
 
-// this is the ARROW function:
+// 3. this is the ARROW function:
 
 const sum = (a, b) => a + b;
 console.log(sum(10, 20)); // output: 30
