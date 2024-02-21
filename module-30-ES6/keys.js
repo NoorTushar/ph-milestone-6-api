c86eb82c6f86f12316f392f8af0f116329969d54
NoorTushar <tushar.noor.khan@gmail.com>
2024-02-22 01:46:17 +0600

    keys

--- module-30-ES6/keys.js
+++ module-30-ES6/keys.js
@@ -5,8 +5,55 @@ const glass = {
    isCleaned: true,
 };
 
+// 01. To get the keys
+
 const keys = Object.keys(glass);
 
 console.log(keys); // [ 'name', 'color', 'price', 'isCleaned' ]
 
 console.log(glass[keys[0]]); // golden glass
+
+// 02. To get the values
+
+const values = Object.values(glass);
+console.log(values); // [ 'golden glass', 'golden', 12, true ]
+
+// 03. To create an array of keys and values: Two Dimensional Array
+
+const pairs = Object.entries(glass);
+
+console.log(pairs);
+
+/*
+
+[
+  [ 'name', 'golden glass' ],
+  [ 'color', 'golden' ],
+  [ 'price', 12 ],
+  [ 'isCleaned', true ]
+]
+
+*/
+
+// 04. Delete a property using delete keyword
+
+delete glass.isCleaned;
+
+console.log(glass); // { name: 'golden glass', color: 'golden', price: 12 }
+
+// 05. Create a new object by Destructuring
+
+const { price, ...restGlass } = glass;
+
+console.log(restGlass); // { name: 'golden glass', color: 'golden' }
+
+// 06. Freeze an Object. You cannot do anything.
+
+Object.freeze(glass);
+
+glass.source = "Bangladesh";
+delete glass.name;
+glass.color = "red";
+console.log(glass); //{ name: 'golden glass', color: 'golden', price: 12 }
+
+// Notice that no changes took place.
