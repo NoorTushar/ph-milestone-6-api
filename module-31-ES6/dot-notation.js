7501c23ebd10c812f6ad49333c26b69ff5086203
NoorTushar <tushar.noor.khan@gmail.com>
2024-02-23 19:15:53 +0600

    dot notation

---
+++ module-31-ES6/dot-notation.js
@@ -0,0 +1,14 @@
+const person = {
+   name: "Kodom Ali",
+   job: "Badam Khai",
+};
+
+// dot notation
+console.log(person.job); // badam khai
+
+//bracket notation
+console.log(person["job"]); // badam khai
+
+// another way to use bracket notation
+const prop = "job";
+console.log(person[prop]); // badam khai
