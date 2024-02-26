9584d57f5d676dd3486bbf27910d42abaefa8020
NoorTushar <tushar.noor.khan@gmail.com>
2024-02-26 16:33:55 +0600

    advanced concepts

--- javascript-concepts-level2/functions.js
+++ javascript-concepts-level2/functions.js
@@ -12,3 +12,27 @@ function b() {
    var x = 100;
    console.log(x);
 }
+
+/**
+ * At the very first time before even running the code, the JavaScript engine
+ * skims through the whole code first time.
+ * It creates a global "Execution Context".
+ * Execution Context has two parts -
+ * 1. Memory - Variable
+ * 2. Code - Thread of Execution
+ *
+ * 1. Memory Phase:
+ *
+ * x = undefined
+ * a = function{....}
+ * b = function{....}
+ *
+ * 2. Execution Phase:
+ * Line1: x = 1
+ * Line2: New Execution Context is created
+ *
+ *      Memory Phase:
+ *      x = undefined
+ *
+ *      Execution Phase
+ */
