5784fdecc6a44e06953b3cdbdbfece769d57a27a
NoorTushar <tushar.noor.khan@gmail.com>
2024-02-29 13:36:54 +0600

    v8 engine concept

--- module-34-api-phone-hunter/scripts/phone.js
+++ module-34-api-phone-hunter/scripts/phone.js
@@ -9,7 +9,21 @@ const loadPhone = (searchText) => {
 const displayPhones = (phone) => {
    // this is the parent container where each items will be appended
    const phoneContainer = document.getElementById("phone-container");
-   phoneContainer.innerHTML = "";
+   phoneContainer.innerText = "";
+
+   console.log(phone.length);
+
+   // 6. We will only show the "Show All" button only when array
+   // length is above 12 items
+   const showButtonContainer = document.getElementById("showButtonContainer");
+   if (phone.length > 12) {
+      showButtonContainer.classList.remove("hidden");
+   } else {
+      showButtonContainer.classList.add("hidden");
+   }
+   // 5. slice the total phone array to 12 items
+   // because we want to show 12 phones at first search
+   phone = phone.slice(0, 12);
 
    phone.forEach((phone) => {
       // 1. create the div for each item
@@ -40,13 +54,29 @@ const displayPhones = (phone) => {
 
       // 4. now append the phoneCard div to the parent container
       phoneContainer.appendChild(phoneCard);
+
+      // 8. hide the spinner div
+      showSpinner(false);
    });
 };
 
 // search Button
 const handleSearch = () => {
+   showSpinner(true);
+
    const searchField = document.getElementById("searchInput");
    const searchText = searchField.value;
 
    loadPhone(searchText);
+};
+
+// ai function e akta parameter nibo,
+// parameter present thakle bhitore condition dibo show and hide er.
+const showSpinner = (isLoading) => {
+   const spinnerDiv = document.getElementById("spinner-div");
+   if (isLoading) {
+      spinnerDiv.classList.remove("hidden");
+   } else {
+      spinnerDiv.classList.add("hidden");
+   }
 };
---
+++ module-35-more-JS-concepts/module-35-2-V8-Engine.md
@@ -0,0 +1,15 @@
+Better noted in obsidian.
+
+![[Pasted image 20240229132649.png]]
+
+### Mechanism
+
+1. JavaScript Code ->
+2. Parser -> it checks line by line what is written
+3. Abstract Syntax Tree -> koto gula varibale ase, function ase, array ase. amon akta tree banae
+4. Interpreter Ingnition ->
+5. Compiler Turbo Fan ->
+6. Optimized Machine Code ->
+7. Bytecode ->
+8. Machine Code ->
+9. Runs on the CPU
