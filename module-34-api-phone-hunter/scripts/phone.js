ab67fcf9e462cc9c843fee50384e54c9431e2ce5
NoorTushar <tushar.noor.khan@gmail.com>
2024-03-01 21:50:21 +0600

    rokomari API

--- module-34-api-phone-hunter/scripts/phone.js
+++ module-34-api-phone-hunter/scripts/phone.js
@@ -1,29 +1,36 @@
-const loadPhone = (searchText) => {
+const showAllBtn = document.getElementById("showAllBtn");
+
+const loadPhone = (searchText, isShowAll) => {
    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
 
    fetch(url)
       .then((res) => res.json())
-      .then((data) => displayPhones(data.data));
+      .then((data) => displayPhones(data.data, isShowAll));
 };
 
-const displayPhones = (phone) => {
+const displayPhones = (phone, isShowAll) => {
    // this is the parent container where each items will be appended
    const phoneContainer = document.getElementById("phone-container");
    phoneContainer.innerText = "";
 
-   console.log(phone.length);
-
    // 6. We will only show the "Show All" button only when array
    // length is above 12 items
    const showButtonContainer = document.getElementById("showButtonContainer");
-   if (phone.length > 12) {
+   if (phone.length > 12 && !isShowAll) {
       showButtonContainer.classList.remove("hidden");
    } else {
       showButtonContainer.classList.add("hidden");
    }
-   // 5. slice the total phone array to 12 items
+
+   // 9. Show All aikhane aisha finally hit korbe and niche conditional
+   // er shathe add hobe je slice korbe ki korbena.
+
+   // 5. slice the total phone array to 12 items if not 'SHOW ALL'
    // because we want to show 12 phones at first search
-   phone = phone.slice(0, 12);
+
+   if (!isShowAll) {
+      phone = phone.slice(0, 12);
+   }
 
    phone.forEach((phone) => {
       // 1. create the div for each item
@@ -46,7 +53,7 @@ const displayPhones = (phone) => {
             <h2 class="card-title">${phone.phone_name}</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
             <div class="card-actions">
-            <button class="btn btn-primary">Buy Now</button>
+            <button onclick = "handleShowDetails('${phone.slug}')" class="btn btn-primary">Show Details</button>
             </div>
         </div>
       
@@ -61,13 +68,13 @@ const displayPhones = (phone) => {
 };
 
 // search Button
-const handleSearch = () => {
+const handleSearch = (isShowAll) => {
    showSpinner(true);
 
    const searchField = document.getElementById("searchInput");
    const searchText = searchField.value;
 
-   loadPhone(searchText);
+   loadPhone(searchText, isShowAll);
 };
 
 // ai function e akta parameter nibo,
@@ -78,4 +85,25 @@ const showSpinner = (isLoading) => {
    isLoading
       ? spinnerDiv.classList.remove("hidden")
       : spinnerDiv.classList.add("hidden");
+};
+
+// 9. handle Show all
+function handleShowAll() {
+   handleSearch(true);
+}
+
+// 10. handle show Detail
+
+const handleShowDetails = (id) => {
+   fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+      .then((res) => res.json())
+      .then((data) => showPhoneDetails(data.data));
+};
+
+const showPhoneDetails = (phone) => {
+   // this will pop up the modal
+   modal_show_details.showModal();
+
+   // now as you have got the phone details as well as modal
+   // modify your code to make it dynamic.
 };
